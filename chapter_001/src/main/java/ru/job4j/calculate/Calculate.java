dce9f48c28d2b25145bd3d0d7681e0ba59cd66d6
igorshirokov93 <mailto:freelancerigor@yandex.ru>
2018-07-03 16:16:32 +0300

    1.3. JavaDoc. Code convention [#4087]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+/**
+*Calculate.
+*
+*@author Igor Shirokov (freelancerigor@yandex.ru)
+*/
+public class Calculate {
+	/**
+	*Main.
+	*@param args - args.
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}	
