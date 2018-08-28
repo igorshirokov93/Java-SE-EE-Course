09036fee40f3c42b162a2e2e336887e5cc0b2a21
igorshirokov93 <mailto:freelancerigor@yandex.ru>
2018-08-28 20:18:21 +0300

    Логика игры (второй вариант)

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -5,103 +5,67 @@ import java.util.function.Predicate;
 /**
  * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
  * @version $Id$
- * @since 23.08.2018.
+ * @since 28.08.2018.
  */
 
 public class Logic3T {
-
-    /**
-     * Игровое поле.
-     */
-
     private final Figure3T[][] table;
-
-    /**
-     * Конструктор, инициализирующий игровое поле.
-     *
-     * @param table Игровое поле.
-     */
-
     public Logic3T(Figure3T[][] table) {
         this.table = table;
     }
-
-    /**
-     * Проверяется, выиграл ли игрок Х.
-     *
-     * @return Результат проверки.
-     */
-
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, this.table.length - 1, -1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, this.table.length - 1, 0, -1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
-    }
-
-    /**
-     * Проверяется, выиграл ли игрок O
-     *
-     * @return Результат проверки.
-     */
-
-    public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, this.table.length - 1, -1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, this.table.length - 1, 0, -1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
-    }
-
-    /**
-     * Метод проверяет выигрышные позиции.
-     *
-     * @param predicate Проверяемое состояние игрового поля.
-     * @param startX    Координата Х начальной точки.
-     * @param startY    Координата Y начальной точки.
-     * @param deltaX    Изменение координаты по Х.
-     * @param deltaY    Изменение координаты по Y.
-     * @return Результат проверки.
-     */
-    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
+        boolean result = false;
+        for (int index = 0; index < table.length - 1; index++) {
+            if (table[index][index] == table[index + 1][index + 1]) {
+                result = true;
                 break;
             }
-        }
-
-        return result;
-    }
-
-    /**
-     * Проверяется, есть ли свободные поля.
-     *
-     * @return Результат проверки.
-     */
-    public boolean hasGap() {
-        boolean hasFree = false;
-        outterLoop:
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                if (!table[i][j].hasMarkO() && !table[i][j].hasMarkX()) {
-                    hasFree = true;
-                    break outterLoop;
+            if (table[table.length - 1 - index][index] == table[table.length - 2 - index][index + 1]) {
+                result = true;
+                break;
+            }
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table.length; j++) {
+                    if (table[i][j] == table[i][index + 1]){
+                        result = true;
+                        break;
+                    }
                 }
             }
         }
-        return hasFree;
+        return result;
     }
-}
-
+    public boolean isWinnerO() {
+        boolean result = false;
+        for (int index = 0; index < table.length - 1; index++) {
+            if (table[index][index] == table[index + 1][index + 1]) {
+                result = true;
+                break;
+            }
+            if (table[table.length - 1 - index][index] == table[table.length - 2 - index][index + 1]) {
+                result = true;
+                break;
+            }
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table.length; j++) {
+                    if (table[i][j] == table[i][index + 1]){
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    public boolean hasGap() {
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if (table[i][j].hasMarkX() == table[i][j].hasMarkO()) {
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+} 
