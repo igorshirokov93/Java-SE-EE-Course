9d88981387ba5f0f5126a23f2b8afce595667c1f
igorshirokov93 <freelancerigor@yandex.ru>
2019-10-25 19:59:46 +0300

    3.1. Разбор первой программы. [#159033]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j.calculate;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Igor Shirokov");
+        System.out.println("11.09.1993");
+    }
+}
