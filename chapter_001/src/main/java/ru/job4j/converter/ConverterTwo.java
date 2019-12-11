97bdd14f026714bb245fc0424e759484892a682e
igorshirokov93 <freelancerigor@yandex.ru>
2019-12-11 23:33:52 +0300

    0. Что такое тестирование. [#159431]

--- chapter_001/src/main/java/ru/job4j/converter/ConverterTwo.java
+++ chapter_001/src/main/java/ru/job4j/converter/ConverterTwo.java
@@ -19,13 +19,25 @@ public class ConverterTwo {
     }
 
     public static void main(String[] args) {
-        int euro = rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        int dollar = rubleToDollar(180);
-        System.out.println("180 rubles are " + dollar + " dollars.");
-        int ruble = euroToRuble(6);
-        System.out.println("6 euro are " + ruble + " rubles.");
-        int rub = dollarToRuble(5);
-        System.out.println("5 dollars are " + rub + " rubles.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in1 = 180;
+        int expected1 = 3;
+        int out1 = rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("180 rubles are 3. Test result : " + passed1);
+        int in2 = 6;
+        int expected2 = 420;
+        int out2 = euroToRuble(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("6 euro are 420. Test result : " + passed2);
+        int in3 = 5;
+        int expected3 = 300;
+        int out3 = dollarToRuble(in3);
+        boolean passed3 = expected3 == out3;
+        System.out.println("5 dollars are 300. Test result : " + passed3);
     }
 }
