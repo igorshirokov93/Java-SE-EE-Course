a7e49b0abc7ffd8534813a86b0d57e3477f38bd9
igorshirokov93 <freelancerigor@yandex.ru>
2020-02-13 17:28:22 +0300

    9.1. String [#237407]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+/**
+ * @author Igor Shirokov (freelancerigor@yandex.ru)
+ * @version $Id$
+ * @since 12.02.2020
+ */
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java";
+        int year = 2020;
+        System.out.println(idea + ", but I am a newbie " + year);
+    }
+}
