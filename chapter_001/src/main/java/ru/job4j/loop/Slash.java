410f113ea42f0a6ffb32fb243ced8028e815dd12
igorshirokov93 <freelancerigor@yandex.ru>
2020-03-23 16:29:29 +0300

    5.9. Крест в псевдографике. [#177182]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,38 @@
+package ru.job4j.loop;
+
+/**
+ * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
+ * @version $1$
+ * @since 23.03.2020
+ */
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell;
+                boolean right = (row + cell) == size - 1;
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+        System.out.println("Draw by 7");
+        draw(7);
+        System.out.println("Draw by 9");
+        draw(9);
+    }
+}
