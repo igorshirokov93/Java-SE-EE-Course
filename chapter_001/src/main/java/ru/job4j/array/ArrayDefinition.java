9a65536e7bcb61ff1d30505c3dec09556e83c9d9
igorshirokov93 <freelancerigor@yandex.ru>
2020-03-24 18:13:31 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+/**
+ * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
+ * @version $1$
+ * @since 24.03.2020.
+ */
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
