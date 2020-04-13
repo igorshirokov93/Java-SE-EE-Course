e1d1d69ebd72102fb51dc14e2bf40aab3b8a0be8
igorshirokov93 <freelancerigor@yandex.ru>
2020-04-13 18:58:39 +0300

    6.7.4. Выигрышные комбинации в сокобан [поправочка]

--- chapter_001/src/main/java/ru/job4j/array/MatrixMono.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixMono.java
@@ -39,7 +39,7 @@ public class MatrixMono {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        for (int index = 0; index < 5; index++) {
+        for (int index = 0; index < board.length; index++) {
             if (monoHorizontal(board, index) || monoVertical(board, index)) {
                 result = true;
                 break;
