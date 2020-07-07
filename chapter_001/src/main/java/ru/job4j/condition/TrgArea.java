57caa76d1007eecff1a844b2ae89e98d71ee31c0
igorshirokov93 <freelancerigor@yandex.ru>
2020-07-07 17:39:36 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

--- chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,20 +2,40 @@ package ru.job4j.condition;
 
 /**
  * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
- * @version $2$
- * @since 09.12.2019
+ * @version $?$
+ * @since 07.07.2020
  */
 
 public class TrgArea {
-    public static double area(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    private PointTwo first;
+    private PointTwo second;
+    private PointTwo third;
+
+    public TrgArea(PointTwo ap, PointTwo bp, PointTwo cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
 
-    public static void main(String[] args) {
-        double rsl = TrgArea.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + rsl);
-        double six = TrgArea.area(3, 4, 5);
-        System.out.println("area (3, 4, 5) = " + six);
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+
+    public boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc && ab + bc > ac && ac + bc > ab);
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
     }
 }
